f0fcbf5b4039ce510d26dccddbd480eae9812855
Gustavo Almeida <gustavoalmeida40@hotmail.com>
2023-07-21 09:56:03 -0300

    book validation added.

--- BookManagement.Business/CoverBusiness.cs
+++ BookManagement.Business/CoverBusiness.cs
@@ -1,4 +1,5 @@
 ﻿
+using BookManagement.Model.Exceptions;
 using BookManagement.Model.Interfaces;
 
 namespace BookManagement.Business
@@ -6,10 +7,12 @@ namespace BookManagement.Business
     public class CoverBusiness : ICoverBusiness
     {
         public readonly ICoverRepository _coverRepository;
+        public readonly IBookRepository _bookRepository;
 
-        public CoverBusiness(ICoverRepository coverRepository)
+        public CoverBusiness(ICoverRepository coverRepository, IBookRepository bookRepository)
         {
             _coverRepository = coverRepository;
+            _bookRepository = bookRepository;
         }
 
         public void UploadFile(MemoryStream file, Guid id)
@@ -19,6 +22,9 @@ namespace BookManagement.Business
 
         public async Task<MemoryStream> GetImage(Guid id)
         {
+            if(!_bookRepository.CheckIfInserted(id))
+                throw new InvalidBookException($"The {id} book doens't exist in the database. Please, check the id.");
+
             return await _coverRepository.GetImage(id);
         }
 
